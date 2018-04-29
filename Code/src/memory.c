f9a1b2ad1321a7e8b6df3a9621f4affd06194017
OlivierAuffan <olivierora@gmail.com>
2018-04-29 12:07:37 +0200

    Gestion taille des bandes

--- Code/src/memory.c
+++ Code/src/memory.c
@@ -2,12 +2,12 @@
 #include <shalw.h>
 
 void alloc(void) {
-  hFil = (double *) calloc(2*size_x*size_y, sizeof(double));
-  uFil = (double *) calloc(2*size_x*size_y, sizeof(double));
-  vFil = (double *) calloc(2*size_x*size_y, sizeof(double));
-  hPhy = (double *) calloc(2*size_x*size_y, sizeof(double));
-  uPhy = (double *) calloc(2*size_x*size_y, sizeof(double));
-  vPhy = (double *) calloc(2*size_x*size_y, sizeof(double));
+  hFil = (double *) calloc(2*w*size_y, sizeof(double));
+  uFil = (double *) calloc(2*w*size_y, sizeof(double));
+  vFil = (double *) calloc(2*w*size_y, sizeof(double));
+  hPhy = (double *) calloc(2*w*size_y, sizeof(double));
+  uPhy = (double *) calloc(2*w*size_y, sizeof(double));
+  vPhy = (double *) calloc(2*w*size_y, sizeof(double));
 }
 
 void dealloc(void) {
